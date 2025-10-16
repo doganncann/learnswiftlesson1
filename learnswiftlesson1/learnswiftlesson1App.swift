88a3cf1ef10945ebf951842884e06877690899d7
canncann0 <turgutdogancan0@gmail.com>
2025-10-16 23:02:22 +0300

    Initial Commit

---
+++ learnswiftlesson1/learnswiftlesson1App.swift
@@ -0,0 +1,17 @@
+//
+//  learnswiftlesson1App.swift
+//  learnswiftlesson1
+//
+//  Created by Dogancan turgut on 16.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct learnswiftlesson1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
